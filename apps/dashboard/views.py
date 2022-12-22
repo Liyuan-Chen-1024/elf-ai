cb1afc6a8865159078d25672da5bae86a062723e
Liyuan Chen <liyuan.chen.1024@gmail.com>
2022-12-22 23:28:17 +0100

    update

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import time
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -14,7 +14,25 @@ from rest_framework.decorators import authentication_classes, permission_classes
 def deploy_service(request, service):
     queue_file = os.path.expanduser('/deployservice/queue')
     
-    with open(queue_file, "a") as queue:
-        queue.write(service+"\n")
+    current_queue = {}
+    current_queue[service] = int(time.time())
+    print(current_queue)
+    with open(queue_file, "r") as queue:
+        for line in queue.readlines():
+            service, ts = line.split(",")
+            ts = int(ts.strip())
+
+            if ts < time.time() - 120:
+                continue
+
+            if service not in current_queue:
+                current_queue = ts
+            else:
+                if current_queue[service] < ts:
+                    current_queue[service] = ts
+    
+    with open(queue_file, "w") as queue:
+        for item in current_queue.items():
+            queue.write("{0},{1}\n".format(item[0], item[1]))
     
     return HttpResponse('OK')
