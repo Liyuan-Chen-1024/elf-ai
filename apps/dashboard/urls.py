74e03f484888786da3e7515b4317771179978715
Liyuan Chen <liyuan.chen.1024@gmail.com>
2022-12-22 21:51:16 +0100

    update

---
+++ apps/dashboard/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('deploy/<str:service>/', views.deploy_service),
+]
---
+++ apps/dashboard/views.py
@@ -0,0 +1,16 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import subprocess
+
+@api_view(['GET'])
+def deploy_service(request, service):
+    queue_file = os.path.expanduser('/deployservice/queue')
+    
+    with open(queue_file, "a") as queue:
+        queue.write(service+"\n")
+    
+    return HttpResponse('OK')
