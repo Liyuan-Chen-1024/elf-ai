f02c3003ee4ff565243857d51b9e4bc92bab2adc
Liyuan Chen <liyuan.chen.1024@gmail.com>
2022-12-23 01:39:12 +0100

    update

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6r=)@lp+(qmp_lp*@3lr7tp_s@_ajpxs!4juz6@@lhc2^wq9n_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
